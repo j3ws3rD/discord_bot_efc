4258d69be8d2b7d5ddd9d45d2d94e5e29e11d12e
j3ws3rD <j3ws3rd@localhost.localdomain>
2022-10-12 20:09:58 +0530

    token protected
    used dotenv created .env to protect token

--- launcher.py
+++ launcher.py
@@ -1,7 +1,13 @@
 VERSION = "0.0.0"
 import os 
+from dotenv import load_dotenv
+
+# loading discord token
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
 print(os.listdir())
 
 from lib.bot import BOT
 
-BOT.run("ODY0OTEzMTkyMjgyODgyMDU5.YO8XLg.OD_ZsUmZu6wvSst3MCU4Cd0bpXY")
+BOT.run(TOKEN)
